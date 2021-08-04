190784d18f9efe826355f4e063954329c31b6fca
losg <wangyongxi8653@yundaex.com>
2021-08-04 16:37:32 +0800

    修改

--- app/src/main/java/com/losg/bgaphotopicker/MainActivity.java
+++ app/src/main/java/com/losg/bgaphotopicker/MainActivity.java
@@ -1,22 +1,8 @@
 package com.losg.bgaphotopicker;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.losg.bgaphotopicker.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
